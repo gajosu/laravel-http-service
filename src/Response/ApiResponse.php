f3bbd0664ef0beff227dd46aacf9eae16769a65b
Gabriel Gonzalez <gajosu15@gmail.com>
2022-07-16 00:36:15 -0500

    Add new methods to ApiResponse class and tests

--- src/Response/ApiResponse.php
+++ src/Response/ApiResponse.php
@@ -2,11 +2,24 @@
 
 namespace  Gajosu\LaravelHttpClient\Response;
 
+use Illuminate\Support\Collection;
 use Psr\Http\Message\ResponseInterface;
 
 class ApiResponse
 {
-    private $response;
+    /**
+     * The underlying PSR response.
+     *
+     * @var \Psr\Http\Message\ResponseInterface
+     */
+    private ResponseInterface $response;
+
+    /**
+     * The decoded JSON response.
+     *
+     * @var array|null
+     */
+    protected ?array $decoded = null;
 
     /**
      * @param \Psr\Http\Message\ResponseInterface $response
@@ -17,23 +30,95 @@ class ApiResponse
     }
 
     /**
-     * Return json decoded body
+     * Get the body of the response.
      *
-     * @return mixed
+     * @return string
      */
-    public function jsonDecode(): mixed
+    public function body(): string
     {
-        return json_decode($this->response->getBody());
+        return (string) $this->response->getBody();
     }
 
     /**
-     * Data attr to collection
+     * Get the JSON decoded body of the response as an array or scalar value.
      *
+     * @param  string|null  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function json($key = null, $default = null): mixed
+    {
+        if (!$this->decoded) {
+            $this->decoded = json_decode($this->body(), true);
+        }
+
+        if (is_null($key)) {
+            return $this->decoded;
+        }
+
+        return data_get($this->decoded, $key, $default);
+    }
+
+    /**
+     * Get the JSON decoded body of the response as an object.
+     *
+     * @return object|array
+     */
+    public function object(): object|array
+    {
+        return json_decode($this->body(), false);
+    }
+
+    /**
+     * Get the JSON decoded body of the response as a collection.
+     *
+     * @param  string|null  $key
      * @return \Illuminate\Support\Collection
      */
-    public function toCollect(): \Illuminate\Support\Collection
+    public function collect($key = null): Collection
     {
-        return collect($this->jsonDecode());
+        return Collection::make($this->json($key));
+    }
+
+    /**
+     * Get a header from the response.
+     *
+     * @param  string  $header
+     * @return string
+     */
+    public function header(string $header): string
+    {
+        return $this->response->getHeaderLine($header);
+    }
+
+    /**
+     * Get the headers from the response.
+     *
+     * @return array<string, string>
+     */
+    public function headers(): array
+    {
+        return $this->response->getHeaders();
+    }
+
+    /**
+     * Get the status code of the response.
+     *
+     * @return int
+     */
+    public function status(): int
+    {
+        return (int) $this->response->getStatusCode();
+    }
+
+    /**
+     * Get the reason phrase of the response.
+     *
+     * @return string
+     */
+    public function reason()
+    {
+        return $this->response->getReasonPhrase();
     }
 
     /**
@@ -43,7 +128,7 @@ class ApiResponse
      */
     public function __toString(): string
     {
-        return $this->response->getBody()->__toString();
+        return $this->body();
     }
 
     /**
---
+++ tests/Response/ApiResponseTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Gajosu\LaravelHttpClient\Tests\Builders;
+
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Collection;
+use Gajosu\LaravelHttpClient\Tests\TestCase;
+use Gajosu\LaravelHttpClient\Response\ApiResponse;
+use Gajosu\LaravelHttpClient\Request\ApiRequestBuilderFake;
+
+class ApiResponseTest extends TestCase
+{
+    public function testGetBody()
+    {
+        $response = new Response(200, [], 'test');
+
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals('test', $api_response->body());
+    }
+
+    public function testJsonDecode()
+    {
+        $data = [
+            'test' => 'test',
+            'sub' => [
+                'test' => 'test',
+            ],
+        ];
+
+        $response = new Response(200, [], json_encode($data));
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals($data, $api_response->json());
+        $this->assertEquals($data['test'], $api_response->json('test'));
+        $this->assertEquals($data['sub'], $api_response->json('sub'));
+        $this->assertEquals($data['sub']['test'], $api_response->json('sub.test'));
+    }
+
+    public function testJsonDecodeObject()
+    {
+        $data = [
+            'test' => 'test',
+            'sub' => [
+                'test' => 'test',
+            ],
+        ];
+
+        $response = new Response(200, [], json_encode($data));
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals(json_decode(json_encode($data)), $api_response->object());
+    }
+
+    public function testToCollection()
+    {
+        $data = [
+            'test' => 'test',
+            'sub' => [
+                [
+                    'test' => 'test',
+                    'sub' => [
+                        'test' => 'test'
+                    ]
+                ]
+            ],
+        ];
+
+        $response = new Response(200, [], json_encode($data));
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals(new Collection($data), $api_response->collect());
+        $this->assertEquals(new Collection($data['sub']), $api_response->collect('sub'));
+    }
+
+    public function testHeaders()
+    {
+        $response = new Response(200, [
+            'header1' => 'value1',
+            'header2' => 'value2',
+        ]);
+
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals($response->getHeaders(), $api_response->headers());
+        $this->assertEquals($response->getHeader('header1')[0], $api_response->header('header1'));
+    }
+
+    public function testGetStatusCode()
+    {
+        $response = new Response(200);
+
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals(200, $api_response->status());
+    }
+
+    public function testGetReasonPhrase()
+    {
+        $response = new Response(200, [], 'test');
+
+        $api_response = new ApiResponse($response);
+
+        $this->assertEquals('OK', $api_response->reason());
+    }
+}
